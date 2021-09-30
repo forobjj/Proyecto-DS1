d47fb141bdd1fe6deaf0404c646b5fd0cdef9ba7
Camilo H <chernandez@unimayor.edu.co>
2021-09-30 12:16:54 -0500

    Agregando ReactF

---
+++ proyectomintic/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AdminVentas from './components/AdminVentas/AdminVentas';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+       <AdminVentas></AdminVentas>
+       </div>
+    </div>
+  );
+}
+
+export default App;
