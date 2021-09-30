3c462b594be1976eae6fb30716d384369f9ee515
Camilo H <chernandez@unimayor.edu.co>
2021-09-30 12:33:15 -0500

    Creando nuevo componente

---
+++ proyectomintic/src/components/EstadoVenta/EstadoVenta.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './EstadoVenta.module.css';
+
+const EstadoVenta = () => (
+  <div className={styles.EstadoVenta}>
+    EstadoVenta Component
+  </div>
+);
+
+EstadoVenta.propTypes = {};
+
+EstadoVenta.defaultProps = {};
+
+export default EstadoVenta;
