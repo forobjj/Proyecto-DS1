af08dffbea77ca2ebecd44b220dd5ec8e3198cba
Camilo H <chernandez@unimayor.edu.co>
2021-10-02 10:25:05 -0500

    colocando login en components

--- proyectomintic/src/App.js
+++ proyectomintic/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import AdminVentas from './components/AdminVentas/AdminVentas';
 import EstadoVenta from './components/EstadoVenta/EstadoVenta';
 import { useAuth0 } from "@auth0/auth0-react";
-import { LoginButton } from "./Login";
-import { LogoutButton } from "./Logout";
-import { Profile } from "./Profile";
-import logo from './logo.svg';
+import { LoginButton } from "./components/Login/Login";
+import { LogoutButton } from "./components/Login/Logout";
+import { Profile } from "./components/Login/Profile";
+import logo from './components/Login/logo.svg';
 
 function App() {
   const { isAuthenticated } = useAuth0();
