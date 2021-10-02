746c093c2e2936478aa2769a8ae3a5e07b5fdb78
AlexanderArias <90356937+Alexander-Arias-Arbelaez@users.noreply.github.com>
2021-10-02 15:52:11 -0500

    Francisco Alexander Arias Arbeláez

---
+++ proyectomintic/src/components/GestionUsuario/GestionUsuario.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './GestionUsuario.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <body>
+      <h1>Gestion de usuario</h1>
+      <label for="idusuario">ID Usuario</label><br />
+      <input type="tetx" id="idusuario" name="idusuario" required="true" /><br />
+      <label for="cargo">Cargo</label><br />
+      <select id="cargo" name="cargo">
+        <option value="administrador">Administrador</option>
+        <option value="vendedor">Vendedor</option>
+      </select><br />
+      <label for="estadousuario">Estado del Usuario</label><br />
+      <select id="estadousuario" name="estadousuario">
+        <option value="pendiente">Pendiente</option>
+        <option value="autorizado">Autorizado</option>
+        <option value="noautorizado">No Autorizado</option>
+      </select><br /><br />
+      <input type="submit" value="Guardar" />
+      <input type="submit" value="Cancelar" />
+      </body>
+    </React.Fragment>
+  );
+}
+
+export default App;
