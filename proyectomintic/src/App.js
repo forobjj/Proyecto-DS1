9894e0c25ce9230492b4819b0d73add199494b89
lburgos <luisa.burgos@yopmail.com>
2021-10-01 19:40:06 -0500

    integración Login

--- proyectomintic/src/App.js
+++ proyectomintic/src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
 import AdminVentas from './components/AdminVentas/AdminVentas';
 import EstadoVenta from './components/EstadoVenta/EstadoVenta';
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./Login";
+import { LogoutButton } from "./Logout";
+import { Profile } from "./Profile";
+import logo from './logo.svg';
 
 function App() {
-  
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <div className="contenedorAdminVentas">
-        <AdminVentas></AdminVentas>  
+    <div className="AppGeneral">
+      <div className="AppHeader">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {isAuthenticated ? (
+            <>
+              <Profile />
+              <LogoutButton />
+            </>
+          ) : (
+            <LoginButton />
+          )}
+        </header>
       </div>
-      <div className="contenedorEstadoVentas">   
-        <EstadoVenta></EstadoVenta> 
-      </div>
+      {isAuthenticated ? (
+        <div className="AppInterno">
+          <div className="contenedorAdminVentas">
+            <AdminVentas></AdminVentas>
+          </div>
+          <div className="contenedorEstadoVentas">
+            <EstadoVenta></EstadoVenta>
+          </div>
+        </div>
+      ) : (
+        <br></br>
+      )}
     </div>
   );
 }
--- proyectomintic/src/index.js
+++ proyectomintic/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider //Almacena estado de autenticación de los usuarios y provee métodos para loguear y desloguear
+      domain="dev-0-u6kb3f.us.auth0.com"
+      clientId="uUw26a6yz0HnxMPDYqGYXXMuUEELeipv"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
